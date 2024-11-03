25150348933b39eb5a66c9da47593a2587cf46c1
Mohamed <zer_moh@yahoo.com>
2024-11-03 02:04:07 -0500

    commit_1

---
+++ android-demo-lib/src/main/java/com/example/android_demo_lib/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.example.android_demo_lib;
+
+import android.util.Log;
+
+public class HelloWorld {
+
+    public static void main(String[] args){
+
+    }
+
+    public static void printLog(String tag, String message){
+        Log.d(tag,message);
+    }
+
+
+}
